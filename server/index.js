e7e6499c59a5f693ea62bd1e9e054509ccafc6ed
Daniel Dodge <dandantheman87@gmail.com>
2022-01-03 13:30:12 -0700

    attempt 2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,14 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/menus.js'))
+    rollbar.info("javascript file join success ")
+  })
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.css'))
+    rollbar.info("css file join success ")
+  })
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
