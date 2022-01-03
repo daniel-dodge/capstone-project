cd93b820c108250fa56f9a4c163090d52eaa40d6
Daniel Dodge <dandantheman87@gmail.com>
2022-01-03 15:53:23 -0700

    heroku update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
+// const {SERVER_PORT} = process.env
 
 app.use(express.json())
 app.get('/', (req, res) => {
