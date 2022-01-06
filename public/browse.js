e10545c32970017232771deb36b3d7c676b0489f
Daniel Dodge <dandantheman87@gmail.com>
2022-01-06 15:07:59 -0700

    style update

--- public/browse.js
+++ public/browse.js
@@ -13,15 +13,15 @@ const getGames = () => {
         console.log(game)
         let gameElem = 
         `<div class='game-box'>
-        <h1>${game.game_name}
-        <p>Made by ${game.game_username}</p>
+        <h1 class = "game-text">${game.game_name}
+        <p class = "game-text light">Made by ${game.game_username}</p>
         </h1>
-        <h2>Description:
-        <p>${game.game_description}</p>
+        <h2 class = "game-text">Description:
+        <p class = "game-text light">${game.game_description}</p>
         </h2>
-        <h3>Total deaths:${game.total_deaths}</h3>
-        <h3>Average pass rate:${((game.total_completions/game.total_deaths) * 100).toFixed(2)}%</h3>
-        <h3>Fastest Time:${game.game_record} by ${game.record_user}</h3>
+        <h3 class = "game-text light">Total deaths:${game.total_deaths}</h3>
+        <h3 class = "game-text light">Average pass rate:${((game.total_completions/game.total_deaths) * 100).toFixed(2)}%</h3>
+        <h3 class = "game-text light">Fastest Time:${game.game_record} by ${game.record_user}</h3>
         <button onclick="directGame()";">play</button>
         </div>`
         gameDiv.innerHTML += gameElem
